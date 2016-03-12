c0c9216abfafde1049cab70db1600ec86a6efe06
Ian Agpawa <agpawaji@gmail.com>
2016-03-12 03:13:44 +0000

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -17,11 +20,10 @@ class ArticlesController < ApplicationController
 
   end
   
-  
+  ##current user
   def create
-    debugger
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = User.first 
     if @article.save 
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -63,11 +65,17 @@ class ArticlesController < ApplicationController
     
   end
   
-  
-  
   def article_params
     params.require(:article).permit(:title, :description)
   end
+  
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own articles"
+      redirect_to root_path
+    end
+  end
+  
   
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
+  
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -22,13 +25,12 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
-  
+
   end
   
   
   def update
-    @user = User.find(params[:id])
+
     if @user.update(user_params)
       flash[:success] = "Your account was updated succesfully"
       redirect_to articles_path
@@ -41,7 +43,7 @@ class UsersController < ApplicationController
   
   
   def show
-    @user = User.find(params[:id])
+
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
@@ -53,5 +55,15 @@ class UsersController < ApplicationController
   end
   
   
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+  end
   
 end
